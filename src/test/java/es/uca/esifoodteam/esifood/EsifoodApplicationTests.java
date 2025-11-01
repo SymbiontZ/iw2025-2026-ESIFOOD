a37e3d8906de6246335ff72132f88d7d8747c1bc
Juan Manuel Caucelo Rodríguez <caucelojuanma@gmail.com>
2025-11-01 18:23:25 +0100

    Inicializacion proyecto + dependencias (spring initializr)

---
+++ src/test/java/es/uca/esifoodteam/esifood/EsifoodApplicationTests.java
@@ -0,0 +1,13 @@
+package es.uca.esifoodteam.esifood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsifoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
